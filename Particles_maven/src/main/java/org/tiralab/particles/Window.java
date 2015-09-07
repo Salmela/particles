11f6d08665d9db4b86371e9c72ce7b700eae4267
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-09-07 23:07:05 +0300

    Add temporary code for rendering the particles.

--- Particles_maven/src/main/java/org/tiralab/particles/Window.java
+++ Particles_maven/src/main/java/org/tiralab/particles/Window.java
@@ -1,5 +1,7 @@
 package org.tiralab.particles;
 
+import java.util.Random;
+
 import java.awt.event.WindowEvent;
 import java.awt.Frame;
 import java.awt.Graphics;
@@ -7,12 +9,23 @@ import java.awt.AWTEvent;
 
 public class Window extends Frame {
 	static final long serialVersionUID = 0x1424234725l;
+	private Particle p[];
 
 	public Window() {
 		this.setSize(128, 128);
 		this.setResizable(true);
 		this.setTitle("particles-demo");
 		this.enableEvents(AWTEvent.WINDOW_EVENT_MASK);
+
+		int i;
+		Random rand = new Random();
+		this.p = new Particle[256];
+
+		for(i = 0; i < 256; i++) {
+			this.p[i] = new Particle(
+				Math.abs(rand.nextInt()) % 128,
+				Math.abs(rand.nextInt()) % 128);
+		}
 	}
 
 	protected void processWindowEvent(WindowEvent event) {
@@ -23,6 +36,17 @@ public class Window extends Frame {
 			default:
 				System.out.println(event);
 				break;
+		}
+	}
+
+	public void paint(Graphics g) {
+		int i;
+
+		for(i = 0; i < 255; i++) {
+			Particle p = this.p[i];
+			g.fillOval((int)p.getX() - 1,
+			           (int)p.getY() - 1,
+			           2, 2);
 		}
 	}
 }
