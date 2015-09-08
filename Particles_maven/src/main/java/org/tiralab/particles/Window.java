248eed701a6ebdfb2e64d1bf3ebf6026403367f8
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-09-08 13:58:46 +0300

    Implement double buffering for gui.

--- Particles_maven/src/main/java/org/tiralab/particles/Window.java
+++ Particles_maven/src/main/java/org/tiralab/particles/Window.java
@@ -4,12 +4,18 @@ import java.awt.event.WindowEvent;
 import java.awt.Frame;
 import java.awt.Insets;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.FontMetrics;
 import java.awt.AWTEvent;
 
 public class Window extends Frame {
 	static final long serialVersionUID = 0x1424234725l;
 	private GuiListener listener;
+	private Image offscreenBuffer;
+	private Graphics offscreenGraphics;
+
+	/* These are for detecting size changes */
+	private int oldWidth, oldHeight;
 
 	public Window(GuiListener listener) {
 		this.setSize(128, 128);
@@ -30,15 +36,39 @@ public class Window extends Frame {
 		}
 	}
 
-	public void paint(Graphics g) {
+	private void resized() {
+		this.offscreenBuffer = createImage(getWidth(), getHeight());
+		this.offscreenGraphics = offscreenBuffer.getGraphics();
+
+		this.oldWidth = getWidth();
+		this.oldHeight = getHeight();
+	}
+
+	public void update(Graphics g){
+		paint(g);
+	}
+
+	public void paint(Graphics graphics) {
+		Graphics g;
 		Particle[] particles;
-		int i, w, h;
+		int contentWidth, contentHeight, i;
 		String text;
 		Insets windowFrame;
 
-		windowFrame = getInsets();
-		w = this.getWidth() - windowFrame.left - windowFrame.right;
-		h = this.getHeight() - windowFrame.top - windowFrame.left;
+		if(this.offscreenGraphics == null ||
+		   this.oldWidth != getWidth() ||
+		   this.oldHeight != getHeight()) {
+			resized();
+		}
+
+		g = this.offscreenGraphics;
+		g.clearRect(0, 0, getWidth(), getHeight());
+
+		windowFrame   = getInsets();
+		contentWidth  = this.getWidth() -
+		                windowFrame.left - windowFrame.right;
+		contentHeight = this.getHeight() -
+		                windowFrame.top - windowFrame.left;
 		text = listener.getHeaderText();
 		particles = listener.fetchParticles(0, 0, w, h);
 
@@ -52,7 +82,11 @@ public class Window extends Frame {
 		FontMetrics metrics = g.getFontMetrics();
 
 		g.drawString(text,
-		             windowFrame.left + w - metrics.stringWidth(text) - 8,
+		             windowFrame.left + contentWidth - metrics.stringWidth(text) - 8,
 		             windowFrame.top + metrics.getHeight());
+
+		graphics.drawImage(this.offscreenBuffer, 0, 0, this);
+
+		this.repaint();
 	}
 }
