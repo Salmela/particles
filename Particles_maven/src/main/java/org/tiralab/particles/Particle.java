daffde39f47b8c65da17bf7e62c53d6d8dd3837c
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-09-08 21:05:04 +0300

    Add velocity fields to the particles.

--- Particles_maven/src/main/java/org/tiralab/particles/Particle.java
+++ Particles_maven/src/main/java/org/tiralab/particles/Particle.java
@@ -1,7 +1,7 @@
 package org.tiralab.particles;
 
 public class Particle {
-	private float x, y, radius;
+	private float x, y, vx, vy, radius;
 
 	public Particle(float x, float y) {
 		this.set(x, y);
@@ -15,8 +15,21 @@ public class Particle {
 		return this.y;
 	}
 
+	public float getVelocityX() {
+		return this.vx;
+	}
+
+	public float getVelocityY() {
+		return this.vy;
+	}
+
 	public void set(float x, float y) {
 		this.x = x;
 		this.y = y;
+	}
+
+	public void setVelocity(float x, float y) {
+		this.vx = x;
+		this.vy = y;
 	}
 }
