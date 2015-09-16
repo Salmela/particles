69c285380cb2747bfa1ef466c22621cb44fe963b
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-09-16 11:40:59 +0300

    Add way to switch models.

--- Particles_maven/src/main/java/org/tiralab/particles/App.java
+++ Particles_maven/src/main/java/org/tiralab/particles/App.java
@@ -5,17 +5,23 @@ package org.tiralab.particles;
  *
  */
 public class App implements GuiListener {
-	private Model activeModel;
-	private Storage activeStorage;
+	private Model models[], activeModel;
+	private Storage storages[], activeStorage;
 	private Window window;
 
 	public App() {
 		this.window = new Window(this);
 		this.window.setVisible(true);
 
-		this.activeStorage = new DirectStorage();
-		this.activeModel = new Stars();
-		//this.activeModel = new Fluid();
+		this.models = new Model[2];
+		this.storages = new Storage[1];
+
+		this.models[0] = new Stars();
+		this.models[1] = new Fluid();
+		this.storages[0] = new DirectStorage();
+
+		this.activeStorage = this.storages[0];
+		this.activeModel = this.models[0];
 
 		this.activeStorage.setModel(this.activeModel);
 	}
@@ -27,16 +33,46 @@ public class App implements GuiListener {
 	public void keyPress(String key) {
 		char k = key.toCharArray()[0];
 		switch(k) {
-			/*TODO switch model*/
+			/* switch model*/
 			case 'M':
+				this.switchModel();
 				break;
 			/*TODO reset the model */
 			case 'R':
 				break;
-			/*TODO switch acceleration structure */
+			/* switch acceleration structure */
 			case 'A':
+				this.switchStorage();
 				break;
 		}
+	}
+
+	private void switchModel() {
+		Model model = this.models[0];
+		int i;
+
+		/* get next model */
+		for(i = 0; i < this.models.length - 1; i++) {
+			if(this.models[i] == this.activeModel) {
+				model = this.models[i + 1];
+			}
+		}
+		this.activeModel = model;
+		this.activeStorage.setModel(this.activeModel);
+	}
+
+	private void switchStorage() {
+		Storage storage = this.storages[0];
+		int i;
+
+		/* get next acceleration structure */
+		for(i = 0; i < this.storages.length - 1; i++) {
+			if(this.storages[i] == this.activeStorage) {
+				storage = this.storages[i + 1];
+			}
+		}
+		this.activeStorage = storage;
+		this.activeStorage.setModel(this.activeModel);
 	}
 
 	/**
