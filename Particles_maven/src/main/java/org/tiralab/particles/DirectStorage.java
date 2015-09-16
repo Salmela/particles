874512add69572264ed94a75ece5341efac5bcd0
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-09-16 10:42:55 +0300

    Implement naive acceleration structure.

---
+++ Particles_maven/src/main/java/org/tiralab/particles/DirectStorage.java
@@ -0,0 +1,43 @@
+package org.tiralab.particles;
+
+public class DirectStorage implements Storage {
+	private Particle[] array;
+	private Model model;
+
+	public DirectStorage() {
+	}
+
+	public void setModel(Model model)
+	{
+		this.model = model;
+		this.model.setStorage(this);
+
+		this.update();
+	}
+
+	public Particle[] getObjectsAtArea(float x, float y,
+	                               float w, float h) {
+		return array;
+	}
+
+	public Particle getObjectAtPoint(float x, float y) {
+		/*FIXME this is incorrect implementation */
+		return array[0];
+	}
+
+	public void addParticle(Particle particle) {
+		this.update();
+	}
+
+	public void removeParticle(Particle particle) {
+		this.update();
+	}
+
+	public void moveParticle(Particle particle) {
+		this.update();
+	}
+
+	private void update() {
+		this.array = model.getParticles();
+	}
+}
