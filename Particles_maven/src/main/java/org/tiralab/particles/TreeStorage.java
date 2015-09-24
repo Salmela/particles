8d6f1ef60769f40e9aca2b4d6e3c75bfe8b42b8f
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-09-24 23:10:21 +0300

    Implement most of the quadtree structure.

--- Particles_maven/src/main/java/org/tiralab/particles/TreeStorage.java
+++ Particles_maven/src/main/java/org/tiralab/particles/TreeStorage.java
@@ -1,8 +1,14 @@
 package org.tiralab.particles;
 
+import java.util.HashSet;
+
+/**
+ * This acceleration structure stores the particles in quadtree.
+ */
 public class TreeStorage implements Storage {
 	private Particle[] array;
 	private Model model;
+	private QuadTree root;
 
 	private abstract class QuadTree {
 		protected QuadTreeNode parent;
@@ -19,9 +25,16 @@ public class TreeStorage implements Storage {
 
 			this.parent = parent;
 			this.childID = childID;
-			this.size = parent.size / 2.0f;
-			this.x = parent.x;
-			this.y = parent.y;
+			if(parent == null) {
+				this.size = 512.0f;
+				this.x = 0f;
+				this.y = 0f;
+				return;
+			} else {
+				this.size = parent.size / 2.0f;
+				this.x = parent.x;
+				this.y = parent.y;
+			}
 
 			r = this.size / 2.0f;
 
@@ -52,8 +65,10 @@ public class TreeStorage implements Storage {
 			return this.parent;
 		}
 
+		abstract public void reset();
 		abstract public void insert(Particle particle);
 		abstract public void remove(Particle particle);
+		abstract public void get(HashSet<Particle> set, float x, float y, float width, float height);
 	}
 
 	private class QuadTreeLeaf extends QuadTree {
@@ -65,6 +80,13 @@ public class TreeStorage implements Storage {
 
 			this.particleCount = 0;
 			this.particles = new Particle[16];
+		}
+
+		/**
+		 * Remove all particles from this leaf.
+		 */
+		public void reset() {
+			this.particleCount = 0;
 		}
 
 		/**
@@ -83,6 +105,7 @@ public class TreeStorage implements Storage {
 					node.insert(this.particles[i]);
 				}
 				node.insert(particle);
+				return;
 			}
 			this.particles[this.particleCount] = particle;
 			this.particleCount++;
@@ -98,18 +121,33 @@ public class TreeStorage implements Storage {
 			for(i = 0; i < this.particleCount; i++) {
 				if(this.particles[i] == particle) break;
 			}
-			if(this.particles[i] != particle) return;
+			if(i == this.particleCount) {
+				System.out.println("The particle was attempted to be removed from incorrect leaf." + particle.getPrevX() +","+ particle.getPrevX());
+				//System.exit(-1);
+				return;
+			}
 
 			/* replace the removed particle with last particle */
 			last = this.particles[this.particleCount - 1];
 			this.particles[i] = last;
 			this.particleCount--;
 		}
+
+		public void get(HashSet<Particle> set, float x, float y, float width, float height) {
+			int i;
+
+			for(i = 0; i < this.particleCount; i++) {
+				set.add(this.particles[i]);
+			}
+		}
 	}
 
 	private class QuadTreeNode extends QuadTree {
 		private QuadTree[] childs;
 
+		public QuadTreeNode() {
+			this(null, 0);
+		}
 		public QuadTreeNode(QuadTreeNode parent, int childID) {
 			super(parent, childID);
 
@@ -121,26 +159,99 @@ public class TreeStorage implements Storage {
 			this.childs[3] = new QuadTreeLeaf(this, 3);
 		}
 
+		/**
+		 * Replace a child with another.
+		 *
+		 * This function is used for replacing QuadTreeLeaf with
+		 * QuadTreeNode when the leaf is full.
+		 *
+		 * @param oldChild The old child.
+		 * @param newChild The new child.
+		 */
 		public void replaceChild(QuadTree oldChild, QuadTree newChild) {
-			int i;
+			int childID;
 
-			for(i = 0; i < 4; i++) {
-				if(this.childs[i] == oldChild) {
-					this.childs[i] = newChild;
+			if(oldChild.childID != newChild.childID) {
+				System.out.println("New child must have same childID as the old one.");
+				System.exit(1);
+			}
+
+			childID = oldChild.childID;
+			if(this.childs[childID] != oldChild) {
+				System.out.println("The tree doesn't have the old child.");
+				System.exit(1);
+			}
+
+			this.childs[childID] = newChild;
+		}
+
+		/**
+		 * Figure out which child contains the coorinate.
+		 *
+		 * @param x Point's x coordinate.
+		 * @param y Point's y coordinate.
+		 */
+		private int getChildIdAt(float x, float y) {
+			if(y > this.y) {
+				if(x > this.x) {
+					return CHILD_TR;
+				} else {
+					return CHILD_TL;
+				}
+			} else {
+				if(x > this.x) {
+					return CHILD_BR;
+				} else {
+					return CHILD_BL;
 				}
 			}
 		}
 
+		/**
+		 * Remove all particles from the children.
+		 */
+		public void reset() {
+			childs[0].reset();
+			childs[1].reset();
+			childs[2].reset();
+			childs[3].reset();
+		}
+
 		public void insert(Particle particle) {
-			/*TODO implement */
+			int childID = getChildIdAt(
+				particle.getX(), particle.getY());
+
+			this.childs[childID].insert(particle);
 		}
 
 		public void remove(Particle particle) {
-			/*TODO implement */
+			int childID = getChildIdAt(
+				particle.getX(), particle.getY());
+
+			this.childs[childID].remove(particle);
+		}
+
+		public void get(HashSet<Particle> set, float x, float y, float width, float height) {
+			boolean getTop, getBottom, getLeft, getRight;
+
+			getTop = (y + height > this.y);
+			getBottom = (y < this.y);
+			getLeft = (x + width > this.x);
+			getRight = (x < this.x);
+
+			if(getTop && getRight)
+				childs[CHILD_TR].get(set, x, y, width, height);
+			if(getTop && getLeft)
+				childs[CHILD_TL].get(set, x, y, width, height);
+			if(getBottom && getRight)
+				childs[CHILD_BR].get(set, x, y, width, height);
+			if(getBottom && getLeft)
+				childs[CHILD_BL].get(set, x, y, width, height);
 		}
 	}
 
 	public TreeStorage() {
+		this.root = new QuadTreeNode();
 	}
 
 	public void setModel(Model model)
@@ -152,8 +263,12 @@ public class TreeStorage implements Storage {
 	}
 
 	public Particle[] getObjectsAtArea(float x, float y,
-	                               float w, float h) {
-		return array;
+	                                   float w, float h) {
+		HashSet<Particle> set = new HashSet<Particle>();
+
+		this.root.get(set, x, y, w, h);
+
+		return set.toArray(new Particle[0]);
 	}
 
 	public Particle getObjectAtPoint(float x, float y) {
@@ -161,14 +276,22 @@ public class TreeStorage implements Storage {
 	}
 
 	public void addParticle(Particle particle) {
-		this.updateParticles();
+		this.root.insert(particle);
 	}
 
 	public void removeParticle(Particle particle) {
-		this.updateParticles();
+		this.root.remove(particle);
 	}
 
 	public void updateParticles() {
-		this.array = model.getParticles();
+		Particle[] array;
+		int i;
+
+		this.root.reset();
+		array = model.getParticles();
+
+		for(i = 0; i < array.length; i++) {
+			this.root.insert(array[i]);
+		}
 	}
 }
