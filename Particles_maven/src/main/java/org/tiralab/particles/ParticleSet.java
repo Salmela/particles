c95fa0d2af129eb1d0adc8667cdca8239298a5ca
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-09-25 15:33:04 +0300

    Implement ParticleSet class.

---
+++ Particles_maven/src/main/java/org/tiralab/particles/ParticleSet.java
@@ -0,0 +1,61 @@
+package org.tiralab.particles;
+
+public class ParticleSet {
+	private ParticleArray[] array;
+	private int size;
+
+	public ParticleSet() {
+		this.size = 0;
+		this.array = new ParticleArray[8];
+	}
+
+	private void resize() {
+		ParticleArray[] oldArray;
+		int i;
+
+		oldArray = this.array;
+		this.array = new ParticleArray[oldArray.length * 2];
+
+		for(i = 0; i < oldArray.length; i++) {
+			if(oldArray[i] == null) continue;
+			for(Particle particle : oldArray[i]) {
+				this.add(particle);
+			}
+		}
+	}
+
+	public void add(Particle particle) {
+		ParticleArray bucket;
+		int hash;
+
+		if(this.size >= this.array.length * 3 / 4) {
+			resize();
+		}
+
+		hash = particle.hashCode();
+		hash %= this.array.length;
+
+		bucket = this.array[hash];
+		if(bucket == null) {
+			bucket = new ParticleArray();
+			this.array[hash] = bucket;
+		}
+
+		bucket.add(particle);
+	}
+
+	public Particle[] toArray() {
+		ParticleArray array = new ParticleArray();
+		int i;
+
+		for(i = 0; i < this.array.length; i++) {
+			if(this.array[i] == null) continue;
+
+			for(Particle particle : this.array[i]) {
+				array.add(particle);
+			}
+		}
+
+		return array.toArray();
+	}
+}
