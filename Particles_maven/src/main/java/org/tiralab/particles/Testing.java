13405e6f0339daa3ecd9e266700b801f31d8b014
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-10-07 10:25:08 +0300

    Add commandline options to the cli code.

--- Particles_maven/src/main/java/org/tiralab/particles/Testing.java
+++ Particles_maven/src/main/java/org/tiralab/particles/Testing.java
@@ -3,27 +3,97 @@ package org.tiralab.particles;
 public class Testing {
 	private Model model;
 	private Storage storage;
-	private int frameEnd;
+	private long frameEnd, count;
 
 	public Testing(String[] args) {
-		//this.models[0] = new Stars();
-		//this.models[1] = new Fluid();
-		//this.storages[0] = new DirectStorage();
-		//this.storages[1] = new ArrayStorage();
-		//this.storages[2] = new TreeStorage();
+		int i;
+		for(i = 0; i < args.length;) {
+			i += this.commandExecute(args, i);
+		}
 
 		this.model = new Stars();
 		this.storage = new DirectStorage();
-		this.frameEnd = 250;
+		this.frameEnd = 50;
+		this.count = 100;
 
 		this.storage.setModel(this.model);
+	}
+
+	public int commandExecute(String[] args, int i) {
+		String command = args[i];
+		if(command.charAt(0) != '-' ||
+		   command.charAt(1) != '-') {
+			System.out.println("The command line flag must start with two dashes.");
+			this.printHelp();
+			return -1;
+		}
+
+		command = command.substring(2);
+		if(command.equals("help")) {
+			this.printHelp();
+			return -1;
+		} else if(command.equals("model")) {
+			this.modelSwitch(args[i + 1]);
+		} else if(command.equals("storage")) {
+			this.storageSwitch(args[i + 1]);
+		} else if(command.equals("iterations")) {
+			this.frameEnd = Long.parseLong(args[i + 1], 10);
+		} else if(command.equals("count")) {
+			this.count = Long.parseLong(args[i + 1], 10);
+		} else {
+			System.out.println("Unknown command line flag.");
+			System.exit(1);
+			return -1;
+		}
+		return 2;
+	}
+
+	public void printHelp() {
+		System.out.println("USAGE: ./testing [OPTIONS]");
+		System.out.println("");
+		System.out.println("Arguments:");
+		System.out.println(" --model MODEL\t\tThe model (default is Stars)");
+		System.out.println(" --storage STORAGE\tThe storage (default is Direct)");
+		System.out.println(" --iterations NUMBER\tThe number of iterations to be");
+		System.out.println("\t\t\texecuted (default is 250)");
+		System.out.println(" --count NUMBER\tThe particle count runned");
+		System.out.println("");
+		System.out.println("Model:");
+		System.out.println(" Stars\tNbody simulator");
+		System.out.println(" Fluid\tFluid simulator");
+		System.out.println("");
+		System.out.println("Storages:");
+		System.out.println(" Direct\tNon-accelerated storage");
+		System.out.println(" Array\t2D array as storage");
+		System.out.println(" Tree\tQuadtree based storage");
+		System.exit(1);
+	}
+
+	public void modelSwitch(String model) {
+		model = model.toLowerCase();
+		if(model.equals("stars")) {
+			this.model = new Stars();
+		} else if(model.equals("fluid")){
+			this.model = new Fluid();
+		}
+	}
+
+	public void storageSwitch(String storage) {
+		storage = storage.toLowerCase();
+		if(storage.equals("direct")) {
+			this.storage = new DirectStorage();
+		} else if(storage.equals("array")){
+			this.storage = new ArrayStorage();
+		} else if(storage.equals("tree")){
+			this.storage = new TreeStorage();
+		}
 	}
 
 	public long runOnce() {
 		long startTime, endTime;
 		int i;
 
-		this.model.reset();
+		this.model.reset(this.count);
 
 		startTime = System.currentTimeMillis();
 		for(i = 0; i < this.frameEnd; i++) {
@@ -34,7 +104,7 @@ public class Testing {
 		return (endTime - startTime);
 	}
 
-	public void run() {
+	public long runCompute() {
 		long timeSum, time;
 		int i, tries;
 
@@ -46,7 +116,16 @@ public class Testing {
 		}
 		time = timeSum / tries;
 
-		System.out.println("Time of " + this.frameEnd + " iterations is " + time);
+		return time;
+	}
+
+	public void run() {
+		while(true) {
+			long time = this.runCompute();
+			System.out.println("Time of " + this.frameEnd + " iterations and " + this.count + " particles is " + time + "ms");
+
+			this.count += 100;
+		}
 	}
 
 	public static void main(String[] args) {
